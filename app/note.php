29a126edaf651ecca8bfea828c21c4491391adaa
Sega Madani SY <segasy99@gmail.com>
2022-07-28 23:06:06 +0000

    Update des fichiers cause d'erreur au niveau du repository

--- app/note.php
+++ app/note.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 if(isset($_POST["submi"]))
@@ -14,20 +13,3 @@ if(isset($_POST["submi"]))
 
 }
 ?>
-=======
-<?php 
-
-if(isset($_POST["submi"]))
-{
-    include("connectionBDD.php") ;
-    $note=$_POST["note"]; 
-    if($note>20 || $note<0){echo("Saisissez une note entre 0 et 20");}
-    else
-    {
-        $req="insert into notes values(NULL ,'$note')";
-        $exec=mysqli_query($connect,$req) ; 
-    }
-
-}
-?>
->>>>>>> main
